0e01ce8cf26606e4ad524a8a1ccd23cc29470b6b
Nelson Puglisi <nelson.puglisi@gmail.com>
2018-11-03 16:16:38 -0300

    view implemented

--- app/src/main/kotlin/com/tridente/atddexcersise/MainActivity.kt
+++ app/src/main/kotlin/com/tridente/atddexcersise/MainActivity.kt
@@ -2,11 +2,34 @@ package com.tridente.atddexcersise
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), BillingFormView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        BillingFormLoadingViewState(BillingFormRepositoryService()).load(this)
     }
+
+    override fun showProgress() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    override fun hideProgress() {
+        progressBar.visibility = View.GONE
+    }
+
+    override fun showDocType(docTypeOptions: List<String>) {
+        spinner2.visibility = View.VISIBLE
+        textView.visibility = View.VISIBLE
+        editText2.visibility = View.VISIBLE
+
+        val dataAdapter = ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, docTypeOptions);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.adapter = dataAdapter
+    }
+
 }
